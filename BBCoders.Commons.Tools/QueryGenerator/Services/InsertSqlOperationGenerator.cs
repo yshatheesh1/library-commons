cf2496e56b1f5e0044db9b8623db3ad3ce547353
= <yshatheesh@gmail.com>
2021-11-29 22:12:38 -0600

    test

--- BBCoders.Commons.Tools/QueryGenerator/Services/InsertSqlOperationGenerator.cs
+++ BBCoders.Commons.Tools/QueryGenerator/Services/InsertSqlOperationGenerator.cs
@@ -1,21 +1,22 @@
 using System.Linq;
-using BBCoders.Commons.QueryConfiguration;
+using BBCoders.Commons.Tools.QueryGenerator.Models;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace BBCoders.Commons.Tools.QueryGenerator.Services
 {
     public class InsertSqlOperationGenerator : SqlOperationGenerator
     {
         private const string _modelSuffix = "InsertModel";
-        public InsertSqlOperationGenerator(ISqlGenerationHelper sqlGenerationHelper, ITable operation) :
-        base(sqlGenerationHelper, operation)
+        public InsertSqlOperationGenerator(SqlOperationGeneratorDependencies dependencies, ITable operation) :
+        base(dependencies, operation)
         { }
 
         public override void GenerateSql(IndentedStringBuilder builder)
         {
             var table = DelimitTable(_table.Name, _table.Schema, false);
+            var columnMappings = GetMappings();
+            var autoIncrementColumn = columnMappings.FirstOrDefault(x => x.isAutoIncrement());
             var columns = GetMappings().Where(x => !x.isAutoIncrement());
             var insertColumns = new string[columns.Count()];
             var insertValues = new string[columns.Count()];
@@ -23,10 +24,11 @@ namespace BBCoders.Commons.Tools.QueryGenerator.Services
             {
                 var column = columns.ElementAt(i);
                 insertColumns[i] = DelimitColumn(_table.Name, column.Name, false);
-                insertValues[i] = column.hasDefaultValue() ? $"If({column.Name} IS NULL,DEFAULT({DelimitColumn(_table.Name, column.Name)}), {column.Name})" : column.Name;
+                insertValues[i] = column.hasDefaultValue() ? $"If(@{column.Name} IS NULL,DEFAULT({DelimitColumn(_table.Name, column.Name)}), @{column.Name})" : $"@{column.Name}";
             }
             builder.AppendLine($"INSERT INTO {table} ({string.Join(", ", insertColumns)}) VALUES ({string.Join(", ", insertValues)});");
-            builder.Append("SELECT LAST_INSERT_ID()");
+            if (autoIncrementColumn != null)
+                builder.Append("SELECT LAST_INSERT_ID()");
         }
 
         public override void GenerateModel(IndentedStringBuilder builder)
@@ -44,7 +46,7 @@ namespace BBCoders.Commons.Tools.QueryGenerator.Services
 
             var columns = GetMappings().Where(x => x.isAutoIncrement()).Select(x => x.Name);
             var autoIncrementColumn = _table.Columns.Where(x => columns.Contains(x.Name)).FirstOrDefault();
-            var returnType = autoIncrementColumn != null ? getTypeName(autoIncrementColumn.PropertyMappings.First().Property.ClrType) : "int";
+            var returnType = autoIncrementColumn != null ? getTypeName(autoIncrementColumn.PropertyMappings.First().Property) : "int";
             builder.AppendLine($"public async Task<{returnType}> Insert{modelName}({insertModelName} {insertModelName})");
             builder.AppendLine("{");
             var nonAutoIncrementColumn = _table.Columns.Where(x => x != autoIncrementColumn);
